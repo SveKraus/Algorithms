7ee347da54298cd8cebc63f3eefd28996e09b1e0
SveKraus <kraus.svetlana@gmx.de>
2023-06-23 12:52:41 +0200

    Initial commit

--- src/HW5.java
+++ src/HW5.java
@@ -1,29 +1,24 @@
-import java.util.Arrays;
-
-import static java.awt.geom.Path2D.contains;
 
 public class HW5 {
-    public static int main(String[] args) {
+    public static void main(String[] args) {
         boolean[] vers = {true, true, false, false, false, false, false};
-        boolean toFind = false;
 
-        System.out.println(contains(vers, false));
-        return 0;
+
+        System.out.println(contains(vers));
+
     }
 
-    public static boolean contains(boolean[] vers, boolean b) {
-        boolean a = Arrays.binarySearch(boolean[](vers), b));
-        int l = 0;
-        int r = vers.length - 1;
+    public static int contains(boolean[] vers) {
 
-          for (int i = 0; i < vers.length; i++)
+        for (int i = 0; i < vers.length; i++) {
 
-        if (i == false &&  i+1 == true ){
-            return i;
+            if (vers[i] == false){
+                return i;
+            }
 
-        }else if (r == true ){
-            return -1;
+
         }
+        return -1;
 
     }
 }
--- src/HW5_1.java
+++ src/HW5_1.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class HW5_1 {
     public static void main(String[] args) {
+        int[] arrayFirst = new int[]{100, 112, 256, 349, 770};
+        int[] arraySecond = new int[]{72, 86, 113, 119, 265, 445, 892};
+        System.out.println(Arrays.toString(mergeSortedArrays(arrayFirst, arraySecond)));
+    }
 
-
-        public static int[] mergeSortedArrays ( int[] arr1, int[] arr2){
+        public static int[] mergeSortedArrays( int[] arr1, int[] arr2){
             int m1 = arr1.length;
             int m2 = arr2.length;
             int[] merger = new int[m1 + m2];
