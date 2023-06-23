55c769e052d64fa09b6a164268020a9ba3590428
SveKraus <kraus.svetlana@gmx.de>
2023-06-23 12:26:16 +0200

    Initial commit

---
+++ src/HW5_1.java
@@ -0,0 +1,28 @@
+public class HW5_1 {
+    public static void main(String[] args) {
+
+
+        public static int[] mergeSortedArrays ( int[] arr1, int[] arr2){
+            int m1 = arr1.length;
+            int m2 = arr2.length;
+            int[] merger = new int[m1 + m2];
+            int i = 0, j = 0, n = 0;
+
+            while (i < m1 && j < m2) {
+                if (arr1[i] <= arr2[j]) {
+                    merger[n++] = arr1[i++];
+                } else {
+                    merger[n++] = arr2[j++];
+                }
+            }
+
+            while (i < m1) {
+                merger[n++] = arr1[i++];
+            }
+
+            while (j < m2) {
+                merger[n++] = arr2[j++];
+            }
+            return merger;
+        }
+    }
